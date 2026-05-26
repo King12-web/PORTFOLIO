339de4ca5eb38ae64a30bd1a80a65724db35096f
Boluwatife <bolumattew44@gmail.com>
2026-05-26 05:35:02 -0700

    linked work

---
+++ sript.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+
+    
+
+});
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark-mode");
+
+});
